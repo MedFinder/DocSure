2926a9c70d66471cf6316af452879cba88b29dc7
Opeyemi Agbeja <niyola59@gmail.com>
2025-03-10 21:10:12 +0100

    feat: add Google Tag Manager integration and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { lexendFont } from "@/lib/font";
 import { Toaster } from "sonner";
 import Providers from "@/providers/permission-provider";
 import { Analytics } from '@vercel/analytics/next';
+import { GoogleTagManager } from '@next/third-parties/google';
+import GTMAnalytics from "./GTMAnalytics";
 export const metadata: Metadata = {
   title: "Docsure | Book top rated doctors near me",
   description: "Docsure | Book top rated doctors near me",
@@ -15,10 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="AW-10808779518" />
       <Providers>
         <body className={`${lexendFont.className} font-sans`}>
           {children}
           <Analytics />
+          <GTMAnalytics/>
           <Toaster richColors={true} position="top-right" />
         </body>
       </Providers>
